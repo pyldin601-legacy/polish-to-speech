bee6b2728cb0783d86bdbed0dd6fd0d81dc6a719
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-12-21 18:57:47 +0200

    Initial commit

---
+++ src/client/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from "react";
+import './App.css';
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+
+  const handleChangeText = useCallback(event => {
+    setText(event.target.value);
+  }, []);
+
+  const handleSubmitText = useCallback(() => {
+    setSubmittedText(text);
+  }, [text]);
+
+  return (
+    <div>
+      <input className={"input-text"} type="text" value={text} onChange={handleChangeText} />
+      <button onClick={handleSubmitText}>Submit</button>
+      {submittedText && (
+        <div>
+          <figure>
+            <figcaption>Listen <b>{submittedText}</b>:</figcaption>
+            <audio controls src={`/api/synthesize/${submittedText}`}>
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </figure>
+          <a href={`/api/synthesize/${submittedText}`} target={"__blank"}>Download MP3</a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
